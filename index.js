66fbb148afdf526b1e17258723684841acb6452d
Darlan Alves <me@darlanalv.es>
2022-01-15 06:30:24 +0100

    fix: add .js main file for non-esm projects

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  cache: true,
+  clearMocks: true,
+  collectCoverage: true,
+  maxWorkers: 1,
+  coverageProvider: "babel",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  slowTestThreshold: 1,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  moduleFileExtensions: ["js", "ts"],
+  testEnvironment: "node",
+  verbose: false,
+};
